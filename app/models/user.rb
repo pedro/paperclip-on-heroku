86762f78854145a1bfd93a30c682519b5fafd65b
Pedro Belo <pedro@heroku.com>
2009-03-19 15:09:30 -0700

    set paperclip to use s3 as the storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
-  has_attached_file :photo, :styles => {
-    :thumb  => "80x80#",
-    :medium => "160x160>",
-  }
+  has_attached_file :photo,
+    :styles => {
+      :thumb  => "80x80#",
+      :medium => "160x160>",
+    },
+    :storage => :s3,
+    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+    :path => ":attachment/:id/:style.:extension",
+    :bucket => 'paperclip-demo'
 end
